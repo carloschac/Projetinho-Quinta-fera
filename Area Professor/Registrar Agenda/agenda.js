bc99db203ae659977102af7b34bd509bb808eacc
Ana Chaves <annachav3s@gmail.com>
2022-04-21 20:38:00 -0300

    .

--- Area Professor/Registrar Agenda/agenda.js
+++ Area Professor/Registrar Agenda/agenda.js
@@ -5,6 +5,7 @@ var formNovoEvento = document.getElementById('formNovoEvento');
 var inputNomeAluno = document.getElementById('nomeAluno');
 var inputNomeEvento = document.getElementById('nomeEvento');
 var inputDataEvento = document.getElementById('dataEvento');
+var msgDeErro = document.getElementById('msgDeErro');
 
 function mostrarNovoEvento(){
     // mostrar o formulario quando clicar em novo evento
@@ -16,15 +17,46 @@ function ocultarNovoEvento(){
     novoEvento.classList.add('d-none');
 }
 
-function novoEventoValido(nomeAluno, nomeEvento, dataEvento){
-    if(nomeAluno.trim().length === 0) return false;
-    if(nomeEvento.trim().length === 0) return false;
-    var timestampEvento = Date.parse(dataEvento);
-    if (isNaN(timestampEvento)) return false;
-    var timestampAtual = (new Date()).getTime();
-    if (timestampEvento < timestampAtual) return false;
-    return true;
 
+function novoEventoValido(nomeAluno, nomeEvento, dataEvento){
+    // validar entrada de dados no form
+
+    var validarok = true;
+    var erro = '';
+
+    if(nomeAluno.trim().length === 0) {
+        erro = 'o nome do aluno é obrigatório!';
+        inputNomeAluno.classList.add('is-invalid');
+        validarok = false;
+    } else {
+        inputNomeAluno.classList.remove('is-invalid');
+    }
+
+    if(nomeEvento.trim().length === 0) {
+        erro = 'o nome do evento é obrigatório!';
+        inputNomeEvento.classList.add('is-invalid');
+        validarok = false;
+    }else{
+        inputNomeEvento.classList.remove('is-invalid');
+    }
+
+    var timestampEvento = Date.parse(dataEvento);
+    var timestampAtual = (new Date()).getTime();
+    if (isNaN(timestampEvento) || timestampEvento < timestampAtual) {
+        if (erro.length > 0){
+            erro += '<br>'
+        }
+        erro += 'A data não pode ser em branco e deve estar no futuro';
+        inputDataEvento.classList.add('is-invalid');
+        validarok = false;
+    } else{
+        inputDataEvento.classList.remove('is-invalid');
+    }
+
+    if (!validarok){
+        msgDeErro.innerHTML = erro;
+    }
+    return validarok;
 }
 
 function salvarNovoEvento(event){
@@ -32,7 +64,6 @@ function salvarNovoEvento(event){
     var nomeAluno = inputNomeAluno.value;
     var nomeEvento = inputNomeEvento.value;
     var dataEvento = inputDataEvento.value;
-
     if (novoEventoValido(nomeAluno, nomeEvento, dataEvento)) {
         console.log('ok');
     }else {
