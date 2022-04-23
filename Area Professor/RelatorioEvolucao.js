e480df872cfddc5868cb596f2cb91db824eacee8
Ana Chaves <annachav3s@gmail.com>
2022-04-23 17:32:38 -0300

    relatorio evolucao ok

---
+++ Area Professor/RelatorioEvolucao.js
@@ -0,0 +1,10 @@
+
+function gerarRelatorio1(){
+    document.getElementById('relatorio_disciplina').innerHTML = document.getElementById('disciplina').value;
+    document.getElementById('relatorio_nome').innerHTML = document.getElementById('nome').value;
+    document.getElementById('relatorio_nota1b').innerHTML = document.getElementById('nota1b').value;
+    document.getElementById('relatorio_nota2b').innerHTML = document.getElementById('nota2b').value;
+    document.getElementById('relatorio_frequencia').innerHTML = document.getElementById('frequencia').value;
+    document.getElementById('relatorio_obs').innerHTML = document.getElementById('obs').value;
+    window.print();
+}
