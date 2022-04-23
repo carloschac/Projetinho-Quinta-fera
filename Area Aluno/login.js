a7a8f128c4bf732a6ea2d7d1bbdcb062820bb4e0
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-23 13:03:26 -0300

    commit

--- Area Aluno/login.js
+++ Area Aluno/login.js
@@ -3,9 +3,9 @@ function logar(){
     var login = document.getElementById('login').value;
     var senha = document.getElementById('senha').value;
 
-    if (login == "admin" && senha == "admin") {
-        alert('Logado com sucesso! Clique em OK para ser redirecionado para a Area do Professor')
-        location.href = "AreaProf.html";
+    if (login == "aluno" && senha == "aluno") {
+        alert('Logado com sucesso! Clique em OK para ser redirecionado para a Area do Aluno')
+        location.href = "AreaAluno.html";
     }else{
         alert('Erro! Verifique se os campos estão em branco OU se seu login e senha estão corretos.')
     }
---
+++ Area Aluno/script.js
@@ -0,0 +1,7 @@
+function func1() {
+    window.alert('Falta 1 semana para as provas do primeiro Bimestre, lembre-se de estudar!')
+}
+
+function func2() {
+    window.alert('Atividade Extra ainda não esta disponivel para ingresso! Tente mais tarde!')
+}
