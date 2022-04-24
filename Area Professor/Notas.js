0f0284d9ac339b7528538d9b2f4aa410c89edfdf
Ana Chaves <annachav3s@gmail.com>
2022-04-24 15:07:10 -0300

    .

--- Area Professor/Notas.js
+++ Area Professor/Notas.js
@@ -1,2 +1,5 @@
-
+function gravarNota(){
+    document.getElementById('registro_nome').innerHTML = document.getElementById('nome').value;
+    document.getElementById('registro_nota').innerHTML = document.getElementById('nota').value;
+}
 
