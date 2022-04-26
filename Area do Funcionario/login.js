0cd7c922a100258e4c201e96b796faeec7e6b4d8
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-04-26 15:59:50 -0300

    ajuste login

---
+++ Area do Funcionario/login.js
@@ -0,0 +1,13 @@
+// validação simples do login
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if (login == "secretaria" && senha == "secretaria") {
+        alert('Logado com sucesso! Clique em OK para ser redirecionado para a Area do Aluno')
+        location.href = "AreaAluno.html";
+    }else{
+        alert('Erro! Verifique se os campos estão em branco OU se seu login e senha estão corretos.')
+    }
+}
+
