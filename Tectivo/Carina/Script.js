8fd1452ad1f973550ac7c3c23b1704079f728dfd
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-06-16 22:28:12 -0300

    up

--- Tectivo/Carina/Script.js
+++ Tectivo/Carina/Script.js
@@ -5,3 +5,33 @@ function lançar() {
 function imprimir() {
     window.print();
 }
+
+// Função para puxar Json
+
+var disciplina = [ 
+        
+    {
+        "Disciplina":"Matemática",
+        "Nota" : "5.5", 
+        "Faltas" : "3"
+
+    },
+    {
+        "Disciplina":"Portugues",
+        "Nota" : "7", 
+        "Faltas" : "13"        
+    },
+    {
+        "Disciplina":"Geografia",
+        "Nota" : "9.5", 
+        "Faltas" : "0"        
+    },
+    {
+        "Disciplina":"Artes",
+        "Nota" : "6", 
+        "Faltas" : "1"        
+    }
+ 
+]
+
+console.log(disciplina);
