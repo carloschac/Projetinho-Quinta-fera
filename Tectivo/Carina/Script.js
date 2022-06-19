b26a77f857df686cb2d6a894cca5dd73b22e900b
Carina de Araujo Rosa <carina_rosa_12@hotmail.com>
2022-06-19 11:41:51 -0300

    Update Script.js

--- Tectivo/Carina/Script.js
+++ Tectivo/Carina/Script.js
@@ -16,43 +16,43 @@ function imprimir() {
 
 // Função para puxar Json
 
-var disc = [ 
+// var disc = [ 
         
-    {
-        "Disciplina":"Matemática",
-        "Nota" : "5.5", 
-        "Faltas" : "3"
+//     {
+//         "Disciplina":"Matemática",
+//         "Nota" : "5.5", 
+//         "Faltas" : "3"
 
-    },
-    {
-        "Disciplina":"Portugues",
-        "Nota" : "7", 
-        "Faltas" : "13"        
-    },
-    {
-        "Disciplina":"Geografia",
-        "Nota" : "9.5", 
-        "Faltas" : "0"        
-    },
-    {
-        "Disciplina":"Artes",
-        "Nota" : "6", 
-        "Faltas" : "1"        
-    }
+//     },
+//     {
+//         "Disciplina":"Portugues",
+//         "Nota" : "7", 
+//         "Faltas" : "13"        
+//     },
+//     {
+//         "Disciplina":"Geografia",
+//         "Nota" : "9.5", 
+//         "Faltas" : "0"        
+//     },
+//     {
+//         "Disciplina":"Artes",
+//         "Nota" : "6", 
+//         "Faltas" : "1"        
+//     }
  
-]
+// ]
 
-console.log(disc);
-var tabela = document.querySelector('td');
+// console.log(disc);
+// var tabela = document.querySelector('td');
  
-for (var i = 0; i < disc.length; i++){
-    var myTd = document.createElement('td')
-    var myP = document.createElement('p')
+// for (var i = 0; i < disc.length; i++){
+//     var myTd = document.createElement('td')
+//     var myP = document.createElement('p')
 
-    myTd.textContent = disc[i].Disciplina +' '+ disc[i].Nota
-    myTd.textContent = disc[i].Disciplina +' '+ disc[i].Faltas
+//     myTd.textContent = disc[i].Disciplina +' '+ disc[i].Nota
+//     myTd.textContent = disc[i].Disciplina +' '+ disc[i].Faltas
    
-    tabela.appendChild(myTd)
-    tabela.appendChild(myP)     
-}    
-    console.log(myTd)
+//     tabela.appendChild(myTd)
+//     tabela.appendChild(myP)     
+// }    
+//     console.log(myTd)
