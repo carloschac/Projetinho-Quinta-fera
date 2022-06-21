5c5690b8473ff4a5a99353ffed0140cd76eb7c8d
Ana Chaves <93927006+anachavesrs@users.noreply.github.com>
2022-06-21 20:16:13 -0300

    up

---
+++ Tectivo/Ana/teste1.js
@@ -0,0 +1,10 @@
+
+
+function duvida1(){
+
+    let texto = document.createElement('article');
+    texto.textContent = '<As atividades extras são um módulo da nossa instituição que permite com que os alunos possam desenvolver outras habilidades além da alfabetização. As ativiaddes que temos disponíveis são: futsal, voleibol, natação, programação básica. Para fazer a matrícula basta ir em Area do aluno/ Matricula Atividade Extra. Você pode ter acesso a tabela de valores entrando em contato com a secretaria.'
+
+    document.body.appendChild(texto);
+
+}
